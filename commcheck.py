a8d0219d67e916aae6e695c991d906df35e085af
Ferix8475 <101017749+Ferix8475@users.noreply.github.com>
2022-07-13 11:45:28 -0700

    Update commcheck.py

--- commcheck.py
+++ commcheck.py
@@ -18,7 +18,7 @@ class Messages:
         self.list = list
     '''Maybe some internal conversion to bytes? maybe a superclass to handle many messages in the future'''
     # this might be a stupid idea
-    def convert_to_bytes(self) -> bytearray:
+    def convert_to_bytes(self):
         bytes = bytearray()
         for x in self.list:
             size = SIZES[x[1]]
@@ -32,31 +32,6 @@ class Messages:
                     bytes.append(int(b))
         print(bytes)
         return bytes
-
-    def decode(self, msgFromServer) -> dict:
-        '''
-            Tries to decode a bytearray from the server with the paridgm of the data
-        '''
-        start = 0
-        recievedData = {}
-        for i, sz in enumerate([x[1] for x in self.list]):
-            end = start + SIZES[sz]
-            if start == 0:
-                temp_byte = msgFromServer[start:end]
-                temp_byte = '0x' + str(temp_byte)[4:6]+str(temp_byte)[8:10]
-            if sz == 'CHAR[15]':
-                temp_byte = msgFromServer[start:end]
-                temp_byte = '%' + str(temp_byte)[2:6] + '%' + str(temp_byte)[6:8] + '%' + str(temp_byte)[8:11] \
-                + '%' + str(temp_byte)[11:13] + '%' + str(temp_byte)[13:15] + '%' + str(temp_byte)[15:]
-            elif sz in ['UINT8', 'UINT16', 'UINT32']:
-                temp_byte = int.from_bytes(msgFromServer[start:end], 'big', signed=False)
-            elif sz in ['INT8', 'INT16', 'INT32']:
-                temp_byte = int.from_bytes(msgFromServer[start:end], 'big', signed=True)
-            temp_dict = {self.list[i][0]:temp_byte}
-            recievedData.update(temp_dict)
-            start = end
-        return recievedData
-
 DATA = Messages(['Settings Header', 'UINT16', 0xfffe],
     ['Message ID', 'UINT16', 86],
     ['uint8', 'UINT8', 69],
@@ -65,7 +40,6 @@ DATA = Messages(['Settings Header', 'UINT16', 0xfffe],
     ['int8', 'INT8', -69],
     ['int16', 'INT16', -420],
     ['int32', 'INT32', -69420]) 
-
 bytesToSend = DATA.convert_to_bytes()
 print(list(bytesToSend))
 serverAddressPort   = ('localhost', PORT)
@@ -85,6 +59,45 @@ msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 msg = "Message from Server {}".format(msgFromServer[0])
 print(msg)
 msgFromServer = msgFromServer[0]
+
+DECODER = {
+    'Settings Header': 2,
+    'Message ID': 2,
+    'UINT8': 1,
+    'UINT16': 2,
+    'UINT32' : 4,
+    'INT8': 1,
+    'INT16': 2,
+    'INT32' : 4,
+    'CHAR[15]' : 15,
+    'Status': 4,
+}
+
+start = 0
+
+recievedData = {}
+
+for sz in DECODER:
+    end = start + DECODER.get(sz)
+    if start == 0:
+        temp_byte = msgFromServer[start:end]
+        temp_byte = "0x" + str(temp_byte)[4:6]+str(temp_byte)[8:10]
+    elif sz == "CHAR[15]":
+        # %Y %m %d T %H %M %S
+        temp_byte = msgFromServer[start:end]
+        print(str(temp_byte))
+        # b'20220713T112329'
+        temp_byte = "%" + str(temp_byte)[2:6] + "%" + str(temp_byte)[6:8] + "%" \
+         + str(temp_byte)[8:11] + "%" + str(temp_byte)[11:13] + "%" + str(temp_byte)[13:15] \
+         + "%" + str(temp_byte)[15:]
+    elif 'U' in sz:
+        temp_byte = int.from_bytes(msgFromServer[start:end], 'big', signed=False)
+    else:
+        temp_byte = int.from_bytes(msgFromServer[start:end], 'big', signed=True)
+    temp_dict = {sz:temp_byte}
+    recievedData.update(temp_dict)
+    start = end
+print(recievedData)
 
 # assert isinstance(msgFromServer, bytearray)
 # bytearray.decode(msgFromServer, )
