8e49afa6ae21f3acca8a30cec3e3d244750c1233
jessicafu232 <jessica.fu@kinkaid.org>
2022-07-13 16:21:00 -0500

    3.5 and 3.6

--- MRM_CONTROL_REQUEST.py
+++ MRM_CONTROL_REQUEST.py
@@ -1,5 +1,4 @@
-from classes import Encoder, Decoder, PORT, SIZES
-import socket
+from classes import Encoder, Decoder
 
 ENCODER = Encoder(['Settings Header', 'UINT16', 0x1003],
                 ['Message ID', 'UINT16', 1],
@@ -7,20 +6,8 @@ ENCODER = Encoder(['Settings Header', 'UINT16', 0x1003],
                 ['Reserved', 'UINT16', 3],
                 ['Scan Interval Time', 'UINT32', 10]
                 )
-bytesToSend = ENCODER.convert_to_bytes()
-print(bytesToSend)
-serverAddressPort  = ('localhost', PORT)
-bufferSize = 1024
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-msg = "Message from Server {}".format(msgFromServer[0])
-print(msg)
-msgFromServer = msgFromServer[0]
 
 DECODER = Decoder(['Settings Header', 'UINT16'],
                 ['Message ID', 'UINT16'],
                 ['Status', 'UINT32']
                 )
-print(DECODER.decode(msgFromServer))
