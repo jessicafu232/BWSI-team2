d3225adc2cb0b1ad129b10ea86933eb90123dcfe
tans2006 <102000904+tans2006@users.noreply.github.com>
2022-07-13 17:50:46 -0400

    Update MRM_SERVER_CONNECT_REQUEST.py

--- MRM_SERVER_CONNECT_REQUEST.py
+++ MRM_SERVER_CONNECT_REQUEST.py
@@ -3,7 +3,7 @@ SETTINGS_HEADER = 0x1004
 ENCODER_37 = Encoder(['Settings Header', 'UINT16', 0x1004],
                 ['Message ID', 'UINT16', 1],
                 ['MRM IP Address', 'UINT32', 2],
-                ['MRM IP Port', 'UINT32', 3],
+                ['MRM IP Port', 'UINT16', 3],
                 ['Reserved', 'UINT16', 4])
 
 DECODER_38 = Decoder(['Settings Header', 'UINT16'],
